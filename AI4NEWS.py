d774e5365bd80ccbcb93b51f7e36bd1cec446598
alessandroCiciarelli <36853940+alessandroCiciarelli@users.noreply.github.com>
2021-04-08 18:04:37 +0200

    Add files via upload

--- AI4NEWS.py
+++ AI4NEWS.py
@@ -16,7 +16,7 @@ def nomi_sorgenti():
 	return nomi_sorgenti	
 
 #carico token accesso Api #1
-newsapi = NewsApiClient(api_key='7f10c40670f8482f8bcdda424f763e8e')
+newsapi = NewsApiClient(api_key='d78564fc55a6473c9597520ee6a386e8')
 sorgenti = nomi_sorgenti()
 
 #carico token accesso Api #2
@@ -26,7 +26,7 @@ newsapi2 = NewsApiClient(api_key='e73ec2d966a7418f86ea1ee40e808da9')
 newsapi3 = NewsApiClient(api_key='e6a433d56f744e9bb49d14c9ac86aa3c')
 
 #carico token accesso Api #4
-newsapi4 = NewsApiClient(api_key='d78564fc55a6473c9597520ee6a386e8')
+newsapi4 = NewsApiClient(api_key='7f10c40670f8482f8bcdda424f763e8e')
 
 #carico token accesso Api #5
 newsapi5 = NewsApiClient(api_key='7a8083f426c14292902588162ba1468d')
