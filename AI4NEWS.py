9761fb1c9ca4665005cc0e3addc772a4c522be66
alessandroCiciarelli <36853940+alessandroCiciarelli@users.noreply.github.com>
2022-03-05 17:22:22 +0100

    Update AI4NEWS.py

--- AI4NEWS.py
+++ AI4NEWS.py
@@ -90,7 +90,7 @@ def stampa_Notizie_new(notizie_da_Stapare):
 		#audio_file = open('notizia'+str(i)+'.mp3', 'rb')
 		#audio_bytes = audio_file.read()
 		st.markdown("<center>\
-		<div class='container' style='margin-bottom:15px;width:90%;border:3px inset #b4b6b7;border-width:3px;border-style:inset;border-color:#b4b6b7;border-radius:10px 25px 25px 30px;-moz-border-radius:10px 25px 25px 30px;-webkit-border-radius:10px 25px 25px 30px;padding:15px;margin-bottom: 10px;'>\
+		<div class='row' style='margin-bottom:15px;width:90%;border:3px inset #b4b6b7;border-width:3px;border-style:inset;border-color:#b4b6b7;border-radius:10px 25px 25px 30px;-moz-border-radius:10px 25px 25px 30px;-webkit-border-radius:10px 25px 25px 30px;padding:15px;margin-bottom: 10px;'>\
   		<h3 style='text-align: left;'>"+titolo+"</h3><br><br><br><div class='row'\
   		<div class='col-sm-4'><img src='" + immagine + "' class='figure-img img-fluid rounded' style=''>\
 		</div><div class='col-sm-8' style='text-align: left;'>\
