40f3a25fae3fd7fd497bb66c920363fb665d3e46
alessandroCiciarelli <36853940+alessandroCiciarelli@users.noreply.github.com>
2022-03-05 16:53:53 +0100

    Update AI4NEWS.py

--- AI4NEWS.py
+++ AI4NEWS.py
@@ -91,7 +91,7 @@ def stampa_Notizie_new(notizie_da_Stapare):
 		#audio_bytes = audio_file.read()
 		st.markdown("<center><div class='container' style='width:90%;border:3px inset #b4b6b7;border-width:3px;border-style:inset;border-color:#b4b6b7;border-radius:10px 25px 25px 30px;-moz-border-radius:10px 25px 25px 30px;-webkit-border-radius:10px 25px 25px 30px;padding:15px;margin-bottom: 25px;'>\
 		<h3 style='text-align: left;'>"+titolo+"</h3><div class='row'  style='text-align: center; >\
-		<br><div class='col' style='text-align: center;padding:15px;'><br>\
+		<br><div class='' style='text-align: center;padding:15px;'><br>\
 		<img src='" + immagine + "' class='figure-img img-fluid rounded' style=''>\
 		</div>\
 		<div class='col' style='text-align: left;'>\
